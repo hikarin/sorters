fec1c431c1cc600ebc17f5e144621cdbca25eabe
Hikari Aikawa <hikari@hikar.in>
2016-08-09 11:36:06 +0900

    remove redundant member variable

--- bitonic.h
+++ bitonic.h
@@ -11,6 +11,4 @@ private:
     void bitonic_sub(bool up, int start, int width);
     void bitonic_merge(bool up, int start, int width);
     void bitonic_compare(bool up, int i, int j);
-
-    size_t prev_size;
 };
